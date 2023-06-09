8c5cdd8252c161f240cb8048793387847b7ea10b
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-06-09 21:19:27 +0700

    init: init base repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-function App() {
-  return (
-    <div className='App'>
-      <p>
-        Hello World!
-      </p>
-    </div>
-  );
-}
+import * as React from "react"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import MainView from "./components/view/MainView"
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
 
-export default App;
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState<"light" | "dark">("dark")
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"))
+      },
+    }),
+    []
+  )
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  )
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MainView colorModeContext={ColorModeContext} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
